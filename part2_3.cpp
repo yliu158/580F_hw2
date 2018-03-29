a92cea8bb1686c67f79e7b03fa5ccfea44ae3fb8
yliu158 <yliu158@binghamton.edu>
2018-03-29 01:06:55 -0400

    eng

--- part2_3.cpp
+++ part2_3.cpp
@@ -9,47 +9,35 @@
 using namespace std;
 
 #define NUM_DIMENSIONS 16
-const int N = 100'000;
+const int N = 1'000'000;
 alignas(32) static float points[N][NUM_DIMENSIONS];
-// alignas(32) static float temp[NUM_DIMENSIONS];
 
 int main() {
-
   uniform_real_distribution<float> dist(-1, 1);
-
-  // static float distance_sq_sum;
   static int count = 0;
-  static int notincluded = 0;
-
   double start = omp_get_wtime();
-  // omp_set_num_threads(4);
-  #pragma omp parallel
-  {
-    default_random_engine eng;
-    while (count < N) {
-      float distance_sq_sum = 0.0;
-      #pragma omp for
-      for (int i = 0; i < NUM_DIMENSIONS; ++i) {
-        points[count][i] = dist(eng);
-        distance_sq_sum += points[count][i]*points[count][i];
-      }
-      if (distance_sq_sum <= 1.0) {
-        #pragma omp atomic
-        ++count;
-        printf("Id: %d  count: %d\n", omp_get_thread_num(), count);
-      } else {
-        #pragma omp atomic
-        ++ notincluded;
-      }
+
+   #pragma omp parallel
+    {
+      default_random_engine eng(omp_get_thread_num());
+      while (count < N) {
+        float distance_sq_sum = 0.0;
+        #pragma omp for
+        for (int i = 0; i < NUM_DIMENSIONS; ++i) {
+          points[count][i] = dist(eng);
+          distance_sq_sum += points[count][i]*points[count][i];
+        }
+
+        if (distance_sq_sum <= 1.0) {
+          #pragma omp atomic
+          ++count;
+          printf("Id: %d  count: %d\n", omp_get_thread_num(), count);
+        }
+       }
     }
-  }
+
   double end = omp_get_wtime();
-
   printf("sampling time: %f\n", end-start);
-  printf("notincluded %d\n", notincluded);
-
-
-
 
   return 0;
 }
