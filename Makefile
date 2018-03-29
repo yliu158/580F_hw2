76c7338a898daf888d5770a8e9751561e4fbf902
yliu158 <yliu158@binghamton.edu>
2018-03-29 04:21:05 -0400

    added

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 run:
-	g++ -std=c++14 -fopenmp -mavx part2_3.cpp -o part2
+	g++ -std=c++14 -fopenmp -mavx part2_4.cpp -o part2
 	./part2
--- part2_3.cpp
+++ part2_3.cpp
@@ -9,33 +9,68 @@
 using namespace std;
 
 #define NUM_DIMENSIONS 16
-const int N = 1'000'000;
+const int N = 100;
 alignas(32) static float points[N][NUM_DIMENSIONS];
 
 int main() {
+
   uniform_real_distribution<float> dist(-1, 1);
   static int count = 0;
+  static int threads_num = omp_get_num_threads();
   double start = omp_get_wtime();
 
-   #pragma omp parallel
-    {
-      default_random_engine eng(omp_get_thread_num());
-      while (count < N) {
-        float distance_sq_sum = 0.0;
-        #pragma omp for
+  #pragma omp parallel
+  {
+    int nthread = omp_get_thread_num();
+    default_random_engine eng(nthread);
+    float distance_sq_sum;
+    float temp[NUM_DIMENSIONS];
+    int booked;
+
+    while (count < N) {
+      distance_sq_sum = 0.0;
+
+  #pragma omp for
+      for (int i = 0; i < NUM_DIMENSIONS; ++i) {
+        temp[i] = dist(eng);
+        distance_sq_sum += temp[i]*temp[i];
+      }
+
+      if (distance_sq_sum <= 1.0) {
+
+  #pragma omp critical
+  {
+      booked = count;
+      ++count;
+  }
+
+  #pragma omp for
         for (int i = 0; i < NUM_DIMENSIONS; ++i) {
-          points[count][i] = dist(eng);
-          distance_sq_sum += points[count][i]*points[count][i];
+          points[booked][i] = temp[i];
         }
 
-        if (distance_sq_sum <= 1.0) {
-          #pragma omp atomic
-          ++count;
-          printf("Id: %d  count: %d\n", omp_get_thread_num(), count);
-        }
-       }
+        printf("Id: %d  count: %d\n", nthread, booked);
+      }
     }
+    printf("%d %s\n", nthread," Finished");
+  }
 
+  // #pragma omp for
+  //   for (int i = nthread; i < N; i+=threads_num ) {
+  //     distance_sq_sum = 2.0;
+  //     while (distance_sq_sum > 1.0) {
+  //       distance_sq_sum = 0.0;
+  //       for (int j = 0; j < NUM_DIMENSIONS; ++j) {
+  //         temp[j] = dist(eng);
+  //         distance_sq_sum += temp[j]*temp[j];
+  //       }
+  //     }
+  //     for (int k = 0; k < NUM_DIMENSIONS; ++k) {
+  //       points[i][k] = temp[k];
+  //     }
+  //   }
+// 
+  // }
   double end = omp_get_wtime();
   printf("sampling time: %f\n", end-start);
 
